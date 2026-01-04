8d6f330b1faebaa1ee161e8ba2b49235cecee043
Yasin Mohammed <mohammedyasinym16720@gmail.com>
2026-01-04 18:45:12 +0200

    setup frontend done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <main>
+      <title>Proiect TW</title>
+      <section>
+        <div className="flex flex-col items-center justify-center h-screen">
+          <h1 className="text-4xl font-bold">Proiect TW</h1>
+          <p className="text-lg">This is a project for the TW course.</p>
+          <button className="bg-blue-500 text-white p-2 rounded-md">
+            Click me
+          </button>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
